7b7fd8ea9f7dc1449ebc3b8b5996f163d7aa87fb
abldxtr <110703641+abldxtr@users.noreply.github.com>
2024-05-23 09:02:17 +0000

    clean

--- app/hook/hocks.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-// import { useDispatch } from "@reduxjs/toolkit";
-// import { TypedUseSelectorHook, useSelector } from "react-redux";
-// import type { RootState } from "../component/redux/store";
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-// export const useAppDispatch: () => AppDispatch = useDispatch;
-// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
